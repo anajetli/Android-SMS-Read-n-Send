a5e00416aa449813e99bede90513206f490221a7
Atif Naseem <atif.naseem22@gmail.com>
2017-10-30 09:50:02 +0500

    Update SMSReceiver.java

--- app/src/main/java/com/atifnaseem/smsreadsend/SMSReceiver.java
+++ app/src/main/java/com/atifnaseem/smsreadsend/SMSReceiver.java
@@ -3,6 +3,7 @@ package com.atifnaseem.smsreadsend;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
@@ -20,22 +21,31 @@ public class SMSReceiver extends BroadcastReceiver {
         Bundle bundle = intent.getExtras();
 
 
-        if(bundle != null){
-            Object[] sms = (Object[]) bundle.get(SMS_BUNDLE);
-            String smsMsg = "";
+        if(intent.getAction().equalsIgnoreCase("android.provider.Telephony.SMS_RECEIVED")) {
+            if (bundle != null) {
+                Object[] sms = (Object[]) bundle.get(SMS_BUNDLE);
+                String smsMsg = "";
 
-            for(int i=0; i<sms.length; i++){
-                SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) sms[i]);
+                SmsMessage smsMessage;
+                for (int i = 0; i < sms.length; i++) {
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                        String format = bundle.getString("format");
+                        smsMessage = SmsMessage.createFromPdu((byte[]) sms[i],format);
+                    }else{
+                        smsMessage = SmsMessage.createFromPdu((byte[]) sms[i]);
+                    }
 
-                String msgBody = smsMessage.getMessageBody().toString();
-                String msgAddress = smsMessage.getOriginatingAddress();
 
-                smsMsg += "SMS from : " + msgAddress + "\n";
-                smsMsg += msgBody + "\n";
+                    String msgBody = smsMessage.getMessageBody().toString();
+                    String msgAddress = smsMessage.getOriginatingAddress();
+
+                    smsMsg += "SMS from : " + msgAddress + "\n";
+                    smsMsg += msgBody + "\n";
+                }
+
+                MainActivity inst = MainActivity.Instance();
+                inst.updateList(smsMsg);
             }
-
-            MainActivity inst = MainActivity.Instance();
-            inst.updateList(smsMsg);
         }
     }
 
